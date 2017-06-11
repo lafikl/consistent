9d63c02463e9c05009c47c4e3ae5b95998a263c4
Khalid Lafi <lafi.khalid@gmail.com>
2017-06-11 07:00:48 +0300

    update godoc examples

--- example_test.go
+++ example_test.go
@@ -7,7 +7,27 @@ import (
 	"github.com/lafikl/consistent"
 )
 
-func Example(t *testing.T) {
+func Example_consistent(t *testing.T) {
+	c := consistent.New()
+
+	// adds the hosts to the ring
+	c.Add("127.0.0.1:8000")
+	c.Add("92.0.0.1:8000")
+
+	// Returns the host that owns `key`.
+	//
+	// As described in https://en.wikipedia.org/wiki/Consistent_hashing
+	//
+	// It returns ErrNoHosts if the ring has no hosts in it.
+	host, err := c.Get("/app.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(host)
+}
+
+func Example_bounded() {
 	c := consistent.New()
 
 	// adds the hosts to the ring
